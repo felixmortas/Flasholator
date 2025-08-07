f4d1f99c10cb76c49f04b0bd331a1bfd33408f5f
felix1801 <noiset.lapin@hotmail.fr>
2025-08-07 14:30:55 +0200

    fix userSubscription to keep canTranslate unchanged

--- lib/core/services/subscription_service.dart
+++ lib/core/services/subscription_service.dart
@@ -61,7 +61,6 @@ class SubscriptionService {
       'isSubscribed': true,
       'subscriptionDate': dateStr,
       'subscriptionEndDate': '',
-      'canTranslate': true,
     };
 
     updateUser(updatedData);
