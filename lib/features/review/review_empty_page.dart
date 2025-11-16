010e7e51401386f0fe86337e060f1eb993718258
felix1801 <noiset.lapin@hotmail.fr>
2025-11-16 21:18:17 +0100

    clean code

--- lib/features/review/review_empty_page.dart
+++ lib/features/review/review_empty_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 import 'package:flasholator/config/constants.dart';
 import 'package:flasholator/features/shared/widgets/eraser_button.dart';
